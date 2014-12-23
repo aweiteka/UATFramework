c48bc0a1c3f622fef51ac11a4cbbfa6cc958b20a
aweiteka <aweiteka@redhat.com>
2014-12-23 09:57:05 -0500

    add support for resource.json file to be in alternate dir defined by env var

--- central_ci_dynamic_hosts.py
+++ central_ci_dynamic_hosts.py
@@ -4,8 +4,23 @@
    returns json of provisioned hosts'''
 
 import json
+import os
 
 resource_file = "resources.json"
+# environment variable with path to resource_file
+resource_env_var = "OLDPWD"
+json_data = None
+
+# If resource_file is not found in current working directory
+# try to use the path from resource_env_var
+# assumes filename is resource_file in both cases
+if not os.path.isfile(resource_file):
+    if os.getenv(resource_env_var):
+        resource_file = '/'.join([os.getenv(resource_env_var), resource_file])
+    else:
+        print "Cannot find file '%s' in current working director or env var '%s'" % (resource_file, resource_env_var)
+        exit(1)
+
 json_data = json.load(open(resource_file))
 
 hosts = { 'cihosts': {
