a1559be0ab040d6d36a84b88f0de48eb35ad8505
Ari LiVigni <alivigni@redhat.com>
2015-03-19 13:47:49 -0400

    Cleaned up pep8 and flake8 issues

--- config/central_ci_dynamic_hosts.py
+++ config/central_ci_dynamic_hosts.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
-'''Ansible dynamic inventory script for central CI
-   returns json of provisioned hosts'''
+'''
+    Ansible dynamic inventory script for central CI
+   returns json of provisioned hosts
+'''
 
 import json
 import os
@@ -18,22 +20,20 @@ if not os.path.isfile(resource_file):
     if os.getenv(resource_env_var):
         resource_file = '/'.join([os.getenv(resource_env_var), resource_file])
     else:
-        print "Cannot find file '%s' in current working director or env var '%s'" % (resource_file, resource_env_var)
+        print "Cannot find file '%s' in current working director or env var " \
+              "'%s'" % (resource_file, resource_env_var)
         exit(1)
 
 json_data = json.load(open(resource_file))
 
-hosts = { 'cihosts': {
-          'hosts': []
-          }
-        }
+hosts = {'cihosts': {'hosts': []}}
 
 for host in json_data['resources']:
     if host:
         # Openstack resources
         if 'ip' in host:
             # may need to add user-defined vars here as well. Example:
-            #hosts['cihosts']['vars'] = { 'ansible_ssh_user': 'cloud-user'}
+            # hosts['cihosts']['vars'] = { 'ansible_ssh_user': 'cloud-user'}
             hosts['cihosts']['hosts'].append(host['ip'])
             hosts[host['name']] = [host['ip']]
         # Beaker resources
