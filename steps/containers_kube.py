14f3ad9c22f0549e8d0b93be5d79a196349c8bdf
Aaron Weitekamp <aweiteka@redhat.com>
2015-04-21 18:23:18 -0400

    remove 'host' param from kube step. parameterize kubectl cmd so any component can be checked

--- steps/containers_kube.py
+++ steps/containers_kube.py
@@ -22,32 +22,21 @@ def step_impl(context, rpm, image, host):
                                host,
                                module_args='docker run %s yum install -y %s' % (image, rpm))
 
-@given('"{number}" pods on "{host}"')
-def step_impl(context, number, host):
-    '''check expected number of pods'''
+@given('"{number}" "{component}" are running')
+def step_impl(context, number, component):
+    '''check expected number of components are running
+       where component is pod, service, minion, etc'''
     import json
     r = context.remote_cmd('command',
-                            host,
-                            module_args='kubectl get pods -o json')
-    for i in r:
-        data = json.loads(i['stdout'])
-        # terrible hack. must be a better way
-        if data['items']:
-            assert len(data['items']) is int(number)
-        else:
-            assert int(number) is 0
+                            module_args='kubectl get %s -o json' % component)
+    if r:
+        for i in r:
+            data = json.loads(i['stdout'])
+            # terrible hack. must be a better way
+            if data['items']:
+                assert len(data['items']) is int(number)
+            else:
+                assert int(number) is 0
+    else:
+        assert False
 
-@given('"{number}" services on "{host}"')
-def step_impl(context, number, host):
-    '''check expected number of services'''
-    import json
-    r = context.remote_cmd('command',
-                            host,
-                            module_args='kubectl get services -o json')
-    for i in r:
-        data = json.loads(i['stdout'])
-        # terrible hack. must be a better way
-        if data['items']:
-            assert len(data['items']) is int(number)
-        else:
-            assert int(number) is 0
